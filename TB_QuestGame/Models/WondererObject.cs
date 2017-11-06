d3cf85d64946aecdcff693d338a8cec6aacd818f
Chris Cosnowski <chris.cosnowski@gmail.com>
2017-11-05 20:26:08 -0500

    9th Commit

---
+++ TB_QuestGame/Models/WondererObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB_QuestGame
+{
+    public class WondererObject : GameObject
+    {
+        public override int Id { get; set; }
+        public override string Name { get; set; }
+        public override string Description { get; set; }
+        public override int HomeLocationId { get; set; }
+        public WondererObjectType Type { get; set; }
+        public bool CanInventory { get; set; }
+        public bool IsConsumable { get; set; }
+        public bool IsVisible { get; set; }
+        public int Value { get; set; }
+    }
+}
